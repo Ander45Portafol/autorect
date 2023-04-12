0ee23cfeec9445aa97b580fc58f36d0b4a114acf
Ander45Portafol <anderdeco25@gmail.com>
2023-04-12 07:54:38 -0600

    CRUD models

--- controller/admin/account.js
+++ controller/admin/account.js
@@ -1,1 +1,8 @@
 const USER_API='bussines/dashboard/users.php'
+
+const FileInput=document.getElementById('archivo')
+const Button=document.getElementById('button')
+
+function abriendoFile(){
+    document.getElementById('archivo').click();
+}
