9aaec0709d6629b34a3748361facf4b7186955a2
Ander45Portafol <anderdeco25@gmail.com>
2023-04-20 11:53:56 -0600

    Comentaries in the api

--- api/enitites/dto/category.php
+++ api/enitites/dto/category.php
@@ -2,55 +2,67 @@
 require_once('../../helpers/validator.php');
 require_once('../../enitites/dao/category_queries.php');
 
-class Category extends CategoryQueries{
-    protected $id=null;
-    protected $nombre=null;
-    protected $imagen=null;
-    protected $descripcion=null;
+//Class with dependeces at he Querie's file
+class Category extends CategoryQueries
+{
+    //Atributes to do manipule data
+    protected $id = null;
+    protected $nombre = null;
+    protected $imagen = null;
+    protected $descripcion = null;
 
-    public function setId($value){
+    //Method's set for each atribute
+    public function setId($value)
+    {
         if ($value) {
-            $this->id=$value; 
+            $this->id = $value;
             return true;
-        }else {
+        } else {
             return false;
         }
     }
-    public function setNombre($value){
+    public function setNombre($value)
+    {
         if ($value) {
-            $this->nombre=$value; 
+            $this->nombre = $value;
             return true;
-        }else {
+        } else {
             return false;
         }
     }
-    public function setImagen($value){
+    public function setImagen($value)
+    {
         if ($value) {
-            $this->imagen=$value; 
+            $this->imagen = $value;
             return true;
-        }else {
+        } else {
             return false;
         }
     }
-    public function setDescripcion($value){
+    public function setDescripcion($value)
+    {
         if ($value) {
-            $this->descripcion=$value; 
+            $this->descripcion = $value;
             return true;
-        }else {
+        } else {
             return false;
         }
     }
-    //metodos para obtener valores de los atributos
-    public function getId(){
+    //Method's get for each atribute
+    public function getId()
+    {
         return $this->id;
     }
-    public function getNombre(){
+    public function getNombre()
+    {
         return $this->nombre;
     }
-    public function getImagen(){
+    public function getImagen()
+    {
         return $this->imagen;
     }
-    public function getDescripcion(){
+    public function getDescripcion()
+    {
         return $this->descripcion;
     }
 }
