59e9e26c9ce42c45b870065bb4772b007d292b84
itsRoberOOF <83309227+itsRoberOOF@users.noreply.github.com>
2023-05-04 11:18:05 -0600

    arreglando jquery

--- resources/js/public.js
+++ resources/js/public.js
@@ -15,35 +15,49 @@ window.addEventListener('scroll', function () {
 
 
 /*Function to make the carousel responsive*/
-var multipleCardCarousel = document.querySelector(
-  "#carouselExampleControls"
-);
-if (window.matchMedia("(min-width: 768px)").matches) {
-  $(multipleCardCarousel).removeClass("slide");
-  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-    interval: false,
-  });
-  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-  var cardWidth = $(".carousel-item").width();
-  var scrollPosition = 0;
-  $("#carouselExampleControls .carousel-control-next").on("click", function () {
-    if (scrollPosition < carouselWidth - cardWidth * 4) {
-      scrollPosition += cardWidth;
-      $("#carouselExampleControls .carousel-inner").animate(
-        { scrollLeft: scrollPosition },
-        600
-      );
-    }
-  });
-  $("#carouselExampleControls .carousel-control-prev").on("click", function () {
+function updateCarousel(){
+  var multipleCardCarousel = document.querySelector(
+    "#carouselExampleControls"
+  );
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    multipleCardCarousel.classList.remove("slide");
+    var carouselInner = document.querySelector(".carousel-inner");
+    var carouselWidth = carouselInner.scrollWidth;
+
+    var carouselItems = document.querySelectorAll(".carousel-item");
+    var cardWidth = carouselItems[0].offsetWidth;
+    var scrollPosition = 0;
+
+    var nextButton = document.querySelector("#carouselExampleControls .carousel-control-next");
+    nextButton.addEventListener("click", function() {
+      if (scrollPosition < carouselWidth - cardWidth * 4) {
+        scrollPosition += cardWidth;
+        var carouselInner = document.querySelector("#carouselExampleControls .carousel-inner");
+        carouselInner.scrollTo({
+          left: scrollPosition,
+          behavior: "smooth"
+        });
+      }
+    });
+
+    var prevButton = document.querySelector("#carouselExampleControls .carousel-control-prev");
+    prevButton.addEventListener("click", function() {
     if (scrollPosition > 0) {
       scrollPosition -= cardWidth;
-      $("#carouselExampleControls .carousel-inner").animate(
-        { scrollLeft: scrollPosition },
-        600
-      );
+      var carouselInner = document.querySelector("#carouselExampleControls .carousel-inner");
+      carouselInner.scrollTo({
+        left: scrollPosition,
+        behavior: "smooth"
+      });
     }
-  });
-} else {
-  $(multipleCardCarousel).addClass("slide");
+    });
+  } else {
+    multipleCardCarousel.classList.add("slide");
+  }
 }
+
+updateCarousel();
+
+window.addEventListener("resize", function() {
+  updateCarousel();
+});
